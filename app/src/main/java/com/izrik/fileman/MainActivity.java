e6dcc5c4d4c6dc93c119c153673690d9197325c1
izrik <izrik@izrik.com>
2017-01-04 16:43:59 -0600

    List all of the files in the root directory.

--- app/src/main/java/com/izrik/fileman/MainActivity.java
+++ app/src/main/java/com/izrik/fileman/MainActivity.java
@@ -2,6 +2,10 @@ package com.izrik.fileman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String pwd = "/";
+        File f = new File(pwd);
+        File[] files = f.listFiles();
+
+        ArrayAdapter<File> adapter = new ArrayAdapter<File>(this, R.layout.file_entry, files);
+        ListView listView = (ListView)findViewById(R.id.object_list);
+        listView.setAdapter(adapter);
     }
 }
