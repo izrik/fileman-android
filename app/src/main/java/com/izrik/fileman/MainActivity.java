77e757900fd1e66facdf5010a9e5ccc729e4df91
izrik <izrik@izrik.com>
2017-02-20 13:44:58 -0600

    Open subfolders when clicked on.

--- app/src/main/java/com/izrik/fileman/MainActivity.java
+++ app/src/main/java/com/izrik/fileman/MainActivity.java
@@ -1,7 +1,10 @@
 package com.izrik.fileman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -14,12 +17,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String pwd = "/";
+        String pwd = "";
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            pwd = intent.getStringExtra("pwd");
+        }
+
+        if (pwd == null || pwd == "") pwd = "/";
+
         File f = new File(pwd);
-        File[] files = f.listFiles();
+        final File[] files = f.listFiles();
 
         ArrayAdapter<File> adapter = new ArrayAdapter<File>(this, R.layout.file_entry, files);
         ListView listView = (ListView)findViewById(R.id.object_list);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent1 = new Intent(getApplicationContext(), MainActivity.class);
+                File file = files[position];
+                if (file.isDirectory()) {
+                    intent1.putExtra("pwd", file.toString());
+                    startActivity(intent1);
+                }
+            }
+        });
     }
 }
